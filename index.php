2fb42370c910514f4361c302cd8464296f1393cf
Sheik0o <floriane.petibon@free.fr>
2024-09-16 16:01:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,11 @@
-<!DOCTYPE html>
-<html lang="en">
+ <!DOCTYPE html>
+<html lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Portefolio</title>
+    <title>Portfolio</title>
 </head>
 <body>
-    <p>Bonjour</p>
+    <p>Bonjour c'est maintenant du PHP</p>
 </body>
 </html>
